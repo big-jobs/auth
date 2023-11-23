c94545a6f302c0aadaa0b4699813d2ce164b030d
Shubham <49497708+cartman91@users.noreply.github.com>
2023-11-23 16:00:41 +0530

    dist

---
+++ dist/module.d.ts
@@ -0,0 +1,2 @@
+export * from "/Users/shubhamgarg/GitHub/new-auth/src/module";
+export { default } from "/Users/shubhamgarg/GitHub/new-auth/src/module";
---
+++ dist/module.mjs
@@ -0,0 +1,12 @@
+import jiti from "file:///Users/shubhamgarg/GitHub/new-auth/node_modules/jiti/lib/index.js";
+
+/** @type {import("/Users/shubhamgarg/GitHub/new-auth/src/module")} */
+const _module = jiti(null, {
+  "esmResolve": true,
+  "interopDefault": true,
+  "alias": {
+    "@nuxt-alt/auth": "/Users/shubhamgarg/GitHub/new-auth"
+  }
+})("/Users/shubhamgarg/GitHub/new-auth/src/module.ts");
+
+export default _module;
